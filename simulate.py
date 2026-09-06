9abcbf5e082def1f3a284c478794809086811cd4
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-09-06 20:25:08 +0300

    Adjust the gains for stabilized output

--- simulate.py
+++ simulate.py
@@ -5,15 +5,15 @@ from pid_control import PID
 def main():
 
     # Define the Gains
-    kp = 1.0
-    ki = 0.1
-    kd = 0.4
+    kp = 4.03
+    ki = 0.081
+    kd = 0.7
 
     # Initialize simulation settings
     dt = 0.1 # Change in time
-    target = 10.0
+    target = 30.0
     current_state = 0
-    deadzone = 0.02
+    deadzone = 0.01
 
     # Create PID instance
     pid = PID(kp, ki, kd, dt)
@@ -23,7 +23,7 @@ def main():
     pid.set_deadzone(deadzone)
 
     # Loop
-    STEPS = 10000
+    STEPS = 100
     for step in range(STEPS):
 
         # Give feedback to the controller
@@ -33,22 +33,18 @@ def main():
         output = pid.compute()
 
         # Apply Physics to mimic mechanical movement
-        current_state += output * 0.1
+        current_state += output * dt
 
-        # Calculate the error
+        # Calculate the error after movement (for display)
         error = target - current_state
 
         # Print Telemetry
         print(f"""Loop: {step}
 Error: {error:.3f}
 PID Output: {output:.3f}
-Updated State: {current_state}
+Updated State: {current_state:.3f}
 ------------------------------"""
 )
-
-
-
-
 
 
 if __name__ == "__main__":
