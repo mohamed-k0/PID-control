b14cce51cdb9d82f1b08bfeb5c61fdb73df81772
mohamed-k0 <mohamed.khaled.sh22@gmail.com>
2026-09-06 12:44:02 +0300

    Define a method to set the set point (target)

--- pid_control.py
+++ pid_control.py
@@ -1,19 +1,26 @@
 
 class PID:
+
     # Define the constructor
     def __init__(self, kp, ki, kd, dt = 0.1):
+        # Gains of P, I, D
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.dt = dt
+        # Set the time interval default as "0.1 seconds"
+        self.dt = dt 
 
         # Initialize needed variables
         self.target = 0
         self.feedback = 0
         self.deadzone = 0
 
-        # Variables to track errors and "integrate" them
+        # Variables to track errors and "integrate/derive" them
         self.prev_error = 0
         self.integral = 0
+
+    # Method to set the target
+    def set_target(self, target):
+        self.target = target
 
         
